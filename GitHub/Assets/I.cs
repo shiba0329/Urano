8fdb92e3da2f328f7f8bf57dab4b0eda2710c3f7
YasuiHaruki <Takkharu0304@gmail.com>
2025-04-08 17:30:23 +0900

    I.csのStart変更

--- GitHub/Assets/I.cs
+++ GitHub/Assets/I.cs
@@ -7,7 +7,7 @@ public class I : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Haruki");
     }
 
     // Update is called once per frame
