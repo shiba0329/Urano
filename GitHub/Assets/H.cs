4edf543f10746447d09859aeb864f40e1e552470
Mikan126 <fuusuke1634@gmail.com>
2025-04-08 17:40:58 +0900

    H.csのStart()を変更

--- GitHub/Assets/H.cs
+++ GitHub/Assets/H.cs
@@ -7,7 +7,7 @@ public class H : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Mikan");
     }
 
     // Update is called once per frame
