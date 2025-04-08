144e3a4e1f144c9f8d1d03b62f76b2fd87cdb4ba
YasuiHaruki <Takkharu0304@gmail.com>
2025-04-08 16:29:44 +0900

    D.csのStart変更

--- GitHub/Assets/D.cs
+++ GitHub/Assets/D.cs
@@ -7,7 +7,7 @@ public class D : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Yasui");
     }
 
     // Update is called once per frame
