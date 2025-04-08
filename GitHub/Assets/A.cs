95d15777b3eaa845c685fe1ff261cde4a17cf5b0
Shiba0329 <oouta03292@gmail.com>
2025-04-08 16:28:56 +0900

    Debug.Log追加

--- GitHub/Assets/A.cs
+++ GitHub/Assets/A.cs
@@ -7,7 +7,7 @@ public class A : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Urano");
     }
 
     // Update is called once per frame
