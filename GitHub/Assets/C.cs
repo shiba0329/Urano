5e0d4eede55400a36c429c7b7dab8dfd3b78c0d0
Mikan126 <fuusuke1634@gmail.com>
2025-04-08 16:30:43 +0900

    C.csのStart()を変更

--- GitHub/Assets/C.cs
+++ GitHub/Assets/C.cs
@@ -7,7 +7,7 @@ public class C : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Majima");
     }
 
     // Update is called once per frame
